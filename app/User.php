d6667cf72d42b97aa1359021a955feea1055ef30
Qi, Chengri a <chengri.a.qi@rakuten.com>
2018-10-17 14:54:57 +0800

    change database to mongodb

--- app/User.php
+++ app/User.php
@@ -3,25 +3,13 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
-use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+//use Illuminate\Foundation\Auth\User as Authenticatable;
+use Jenssegers\Mongodb\Auth\User as Authenticatable;
 
-class User extends Eloquent implements
-    AuthenticatableContract,
-    AuthorizableContract,
-    CanResetPasswordContract
+class User extends Authenticatable
 {
     use Notifiable;
-    
-    use \Illuminate\Auth\Authenticatable;
-    use \Illuminate\Foundation\Auth\Access\Authorizable;
-    use \Illuminate\Auth\Passwords\CanResetPassword;
-    
-    protected $connection = 'mongodb';
-    protected $collection = 'users';
-    
+
     /**
      * The attributes that are mass assignable.
      *
